5a6ee6ec59ad276588a7519569c7289abfa9ca06
DmitryS <sidorovdmitriy11@yandex.ru>
2024-04-16 11:59:43 +0300

    wip

--- pkg/services/licensing/oss.go
+++ pkg/services/licensing/oss.go
@@ -4,7 +4,7 @@ import (
 	"github.com/grafana/grafana/pkg/api/dtos"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
 	"github.com/grafana/grafana/pkg/services/hooks"
-	"github.com/grafana/grafana/pkg/services/navtree"
+// 	"github.com/grafana/grafana/pkg/services/navtree"
 	"github.com/grafana/grafana/pkg/setting"
 )
 

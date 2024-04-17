a4a5290da15e75ac2b094e19eb060363deb33cce
DmitryS <sidorovdmitriy11@yandex.ru>
2024-04-17 14:20:17 +0300

    wip

--- pkg/services/navtree/navtreeimpl/navtree.go
+++ pkg/services/navtree/navtreeimpl/navtree.go
@@ -109,7 +109,7 @@ func (s *ServiceImpl) GetNavTree(c *contextmodel.ReqContext, prefs *pref.Prefere
     		dashboardLink := &navtree.NavLink{
     			Text:       "Вагоны в сети",
     			Id:         navtree.NavIDCarsOnline,
-    			Icon:       "rss",
+    			Icon:       "fa fa-wifi",
     			Url:        s.cfg.AppSubURL + "/d/dvRiH6TGz",
     			SortWeight: navtree.WeightDashboard,
     			Children:   carsOnlineChildLinks,
